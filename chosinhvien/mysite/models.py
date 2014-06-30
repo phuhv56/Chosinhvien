dfccf50c08e353d810b723971864e55e5744eb5a
stevendan <danpv1993@gmail.com>
2014-06-30 16:28:19 +0700

    4h28 30/6/2014

--- chosinhvien/mysite/models.py
+++ chosinhvien/mysite/models.py
@@ -2,14 +2,26 @@ from django.db import models
 
 # Create your models here.
 
+class Member(models.Model):
+    username = models.CharField(max_length=30)
+    password = models.TextField()
+    fullname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=75)
+    sex = models.BooleanField(default=0)
+    birthday = models.DateField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
+    facebook = models.CharField(max_length=100)
+    last_login = models.DateTimeField()
+
 
 class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True)
     slug = models.SlugField(unique=True)
-    # user = models.ForeignKey('Member')
+    user = models.ForeignKey('Member')
     category = models.ForeignKey('Category')
-    value = models.IntegerField()
+    price = models.IntegerField()
     image = models.FileField(upload_to='images/upload')
     quantity = models.IntegerField()
     time_post = models.DateField()
@@ -17,6 +29,7 @@ class Product(models.Model):
 
     def __unicode__(self):
         return self.name
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
